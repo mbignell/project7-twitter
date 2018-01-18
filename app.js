a2e8fdaf8bd782e018f96f57a763ce9c8d45e7fe
Maggie Bignell <mbignell@users.noreply.github.com>
2018-01-17 22:01:35 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+
+const app = express();
+
+// Sets port 8080 instead of localhost 3000 due to conflict on my comp
+app.set('port', process.env.PORT || 8080);
+app.listen(app.get('port'));
+//
+// app.use(bodyParser.urlencoded({ extended: false}));
+// app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+app.use(express.static('public'));
+
+// Connects main routes through index.js
+const mainRoutes = require('./js');
+app.use(mainRoutes);
+
+// If route is not found, render 404
+app.use((req,res,next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+})
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
