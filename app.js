ad29e1a07f33e8a6b6155b8d08266fd03d0626d3
Maggie Bignell <mbignell@users.noreply.github.com>
2018-01-19 18:33:24 -0800

    Blargh data calls

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
 const express = require('express');
 const config = require('./config');
 const Twit = require('twit')
+const t = new Twit(config);
 const app = express();
 
-// Sets port 8080 instead of localhost 3000 due to conflict on my comp
+// Sets port 8080 instead of localhost:3000 due to conflict on my comp
 app.set('port', process.env.PORT || 8080);
 app.listen(app.get('port'));
 
+// Sets pugs as view engine
 app.set('view engine', 'pug');
 
+// Sets up static path for images
 const path = require('path')
 app.use(express.static(path.join(__dirname, 'assets')));
 
-// Connects main routes through index.js
-const mainRoutes = require('./js');
+app.use(
+  (req, res, next) => {
+    t.get('statuses/home_timeline', { count: 5 }, function (err, data, response) {
+      req.tweets = data;
+      console.log(req.tweets);
+    });
+    next();
+  }
+)
 
-app.use(mainRoutes);
-//
+app.get('/', function(req, res){
+  const { tweets } = req;
+  res.render('index', { tweets });
+  console.log('ok I got here');
+});
+
+// Get followers
+// t.get('followers/ids', { screen_name: 'maggled' },  function (err, data, response) {
+//   // console.log(data);
+// })
+
+// var stream = t.stream('maggled', { stringify_friend_ids: true })
+// stream.on('direct_message', function (directMsg) {
+//   console.log(directMsg);
+// })
+
 // If route is not found, render 404
 app.use((req,res,next) => {
   const err = new Error('Not Found');
